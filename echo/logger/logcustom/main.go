c4944511b13cea5dd215d66702d325cea2a0fcbe
Dongju <sat0635@naver.com>
2020-03-20 17:12:52 +0900

    Add: e.Logger.SetOutput

--- echo/logger/logcustom/main.go
+++ echo/logger/logcustom/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/labstack/echo"
 )
@@ -10,6 +12,13 @@ func main() {
 	e := echo.New()
 	e.GET("/", Handler)
 	e.Logger.SetLevel(0)
+	t := time.Now()
+	startTime := t.Format("2006_01_02__15_04")
+	f, err := os.Create(startTime + ".log")
+	if err != nil {
+		return
+	}
+	e.Logger.SetOutput(f)
 	e.Logger.SetHeader(`{"time":"${time_rfc3339_nano}","level":"${level}","prefix":"${prefix}",` + `"file":"${short_file}","line":"${line}"}`)
 	e.Start(":6080")
 }
